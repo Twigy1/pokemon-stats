3bbb0e927d8cb90d23aea51b2277f84f15ec1fba
Twigy1 <twigy1@csu.fullerton.edu>
2023-12-19 16:15:26 -0800

    commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Message from "./components/message";
+import List2 from "./components/List2";
+
+function App() {
+
+  return (<><div><List2/></div>
+          </>);
+
+}
+
+export default App;
---
+++ src/components/List2.js
@@ -0,0 +1,56 @@
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import "./List2.css";
+import PokemonCard from "./Card.js";
+import { TextField } from '@mui/material'
+
+function List2(){
+  const [allPokemon, setAllPokemon] = useState([]);
+
+  const getAllPokemon = async () => {
+      const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=700&offset=0");
+      const data = await res.json();
+    
+  
+
+      function createPokemonObject(results){
+        results.forEach(async (pokemon) => {
+          const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`);
+          const data = await res.json();
+          setAllPokemon((currentList) => [...currentList, data]);
+          await allPokemon.sort((a,b) => a.id - b.id);
+        });
+  }
+  createPokemonObject(data.results);
+  console.log(allPokemon)
+}
+
+  useEffect(() =>{
+    getAllPokemon();
+  }, []);
+
+  console.log(allPokemon);
+
+  return <div className="app-container">
+      <div className="pokemon-container">
+        <div className = "all-container">
+          {allPokemon.map((pokemonStats) => ( 
+            <PokemonCard 
+            key={pokemonStats.name}
+            id={pokemonStats.id.toString().padStart(3, "0")}
+            image={pokemonStats.sprites.other["official-artwork"].front_default}
+            name={pokemonStats.name.replace(/^./,(str)=> str.toUpperCase())}
+            type={pokemonStats.types[0].type.name}
+            weight={pokemonStats.weight}
+            height={pokemonStats.height}
+            stats={pokemonStats.stats.map((stat) => stat.base_stat).slice(0,3)} 
+            statsName={pokemonStats.stats.map((stat) => stat.stat.name).slice(0,3)}
+          />
+          ))}
+        </div>
+      </div>
+    </div>
+  
+}
+
+export default List2
